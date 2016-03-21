c345ac4a9e08c264ff2cee6d680da441ca7d33b5
Jane Philipps <jane.philipps@gmail.com>
2016-03-20 21:17:26 -0700

    WIP swappable

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ $(function() {
 
     var cache = $('.box').html();
 
-    $('.box').sortable({
-        connectWith: '.box',
-        cursor: 'move',
-        delay: 150,
-        distance: 5,
-        scroll: false
+    // $('.box').sortable({
+    //     connectWith: '.box',
+    //     cursor: 'move',
+    //     delay: 150,
+    //     distance: 5,
+    //     scroll: false
+    // })
+    $('.box').droppable();
+    $('.draggable-box').draggable({
+        revert: 'invalid'
+    }).droppable({
+        drop: function(event,ui) {
+            swapNodes($(this).get(0),$(ui.draggable).get(0));
+        }
     });
 
+
+    //by bobince - http://stackoverflow.com/a/698440/2033671
+    function swapNodes(a, b) {
+        var aparent= a.parentNode;
+        var asibling= a.nextSibling === b ? a : a.nextSibling;
+        b.parentNode.insertBefore(a, b);
+        aparent.insertBefore(b, asibling);
+    };
+
     $('#reset').click(function() {
-        $('#one').html(cache).sortable('refresh');
+        $('#one').html(cache);
         $('#two').empty();
     });
 
     $('#randomize').click(function() {
-
-        // get containers
-        // initialize empty array for each container
-        // shuffle boxes and depending on number push into either array
 
         var containers = $('.box');
         var boxes = containers.children();
