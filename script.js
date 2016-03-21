f8b0bac7050e21756a859404645bc0f09a943260
Jane Philipps <jane.philipps@gmail.com>
2016-03-20 20:30:02 -0700

    get randomize working

--- script.js
+++ script.js
@@ -11,8 +11,34 @@ $(function() {
     });
 
     $('#reset').click(function() {
-
         $('#one').html(cache).sortable('refresh');
         $('#two').empty();
+    });
+
+    $('#randomize').click(function() {
+
+        // get containers
+        // initialize empty array for each container
+        // shuffle boxes and depending on number push into either array
+
+        var containers = $('.box');
+        var boxes = containers.children();
+
+        var firstContainer = [];
+        var secondContainer = [];
+
+        for (var i = 0; i < boxes.length; i++) {
+            var num = Math.floor(Math.random() * 10);
+
+            if (num < 5) {
+                firstContainer.push(boxes[i]);
+            } else {
+                secondContainer.push(boxes[i]);
+            }
+        }
+        containers.empty();
+        $('#one').append(firstContainer);
+        $('#two').append(secondContainer);
+
     });
 });
